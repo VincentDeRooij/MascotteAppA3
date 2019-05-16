ba9210f440c5ad59ead436e4aca711467449af4f
Vincent <vwjapderooij@gmail.com>
2019-05-16 16:18:03 +0200

    implementation of the MapBox
    Location must be implemented next, this mapbox version contains the possibility to give the map

--- app/src/main/java/com/example/mascotteappa3/MainActivity.java
+++ app/src/main/java/com/example/mascotteappa3/MainActivity.java
@@ -1,13 +1,108 @@
 package com.example.mascotteappa3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.acces_token));
         setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+
+                mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/vwjapderooij/cjvqha9wx0t8w1co93osiftuh"), new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Add the marker image to map
+//                        style.addImage("marker-icon-id",
+//                                BitmapFactory.decodeResource(
+//                                        MainActivity.this.getResources(), R.drawable.mapbox_marker_icon_default));
+//
+//                        SymbolLayer symbolLayer = new SymbolLayer("layer-id", "source-id");
+//                        SymbolLayer symbolLayer2 = new SymbolLayer("layer-id2", "source-id2");
+//                        symbolLayer.withProperties(
+//                                PropertyFactory.iconImage("marker-icon-id")
+//                        );
+//                        symbolLayer2.withProperties(
+//                                PropertyFactory.iconImage("marker-icon-id")
+//                        );
+//                        style.addLayer(symbolLayer);
+//                        style.addLayer(symbolLayer2);
+                    }
+                });
+            }
+        });
+    }
+
+    // Add the mapView's own lifecycle methods to the activity's lifecycle methods
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
